13be7ac8a58dc8bfec4767aaaed59bfdc946893c
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2026-05-19 14:46:08 +0300

    fix tests

--- cvat/apps/dataset_manager/annotation.py
+++ cvat/apps/dataset_manager/annotation.py
@@ -198,7 +198,7 @@ class AnnotationIR:
             for interval in self.intervals:
                 if not self.is_interval_inside(interval, start, stop):
                     raise Exception(
-                        f"Interval {interval.id} cannot be sliced to the [{start}, {stop}] range"
+                        f"Interval {interval['id']} cannot be sliced to the [{start}, {stop}] range"
                     )
             splitted_data.intervals = deepcopy(self.intervals)
 
--- tests/python/rest_api/test_requests.py
+++ tests/python/rest_api/test_requests.py
@@ -497,16 +497,16 @@ class TestGetRequests:
 
         if target_type == "project":
             export_func, import_func = export_project_dataset, import_project_dataset
-            target = next(iter(projects))
+            target = next(p for p in projects if p["dimension"] != "1d")
             owner = target["owner"]
         elif target_type == "task":
             export_func, import_func = export_task_dataset, import_task_annotations
-            target = next(iter(tasks))
+            target = next(t for t in tasks if t["media_type"] != "audio")
             owner = target["owner"]
         else:
             assert target_type == "job"
             export_func, import_func = export_job_dataset, import_job_annotations
-            target = next(iter(jobs))
+            target = next(j for j in jobs if j["media_type"] != "audio")
             owner = tasks[target["task_id"]]["owner"]
 
         target_id = target["id"]
